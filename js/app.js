cd77c165f6dc97e7d762e30448feb9254b885bfa
MasteRminD6666 <rami-zaitoun99@hotmail.com>
2021-07-25 17:58:56 +0300

    fixing the images

--- js/app.js
+++ js/app.js
@@ -32,18 +32,18 @@ new BusMall('boots','./img/boots.jpg') ;
 new BusMall('breakfast','./img/breakfast.jpg') ;
 new BusMall('bubblegum','./img/bubblegum.jpg') ;
 new BusMall('chair','./img/chair.jpg') ;
-new BusMall('cthulhu','/img/cthulhu.jpg') ;
-new BusMall('dog-duck','/img/dog-duck.jpg') ;
-new BusMall('dragon','/img/dragon.jpg') ;
-new BusMall('pen','/img/pen.jpg') ;
-new BusMall('pet-sweep','/img/pet-sweep.jpg') ;
-new BusMall('scissors','/img/scissors.jpg') ;
-new BusMall('shark','/img/shark.jpg') ;
-new BusMall('sweep','/img/sweep.png') ;
-new BusMall('tauntaun','/img/tauntaun.jpg') ;
-new BusMall('unicorn','/img/unicorn.jpg') ;
-new BusMall('water-can','/img/water-can.jpg') ;
-new BusMall('wine-galss','/img/wine-glass.jpg') ;
+new BusMall('cthulhu','./img/cthulhu.jpg') ;
+new BusMall('dog-duck','./img/dog-duck.jpg') ;
+new BusMall('dragon','./img/dragon.jpg') ;
+new BusMall('pen','./img/pen.jpg') ;
+new BusMall('pet-sweep','./img/pet-sweep.jpg') ;
+new BusMall('scissors','./img/scissors.jpg') ;
+new BusMall('shark','./img/shark.jpg') ;
+new BusMall('sweep','./img/sweep.png') ;
+new BusMall('tauntaun','./img/tauntaun.jpg') ;
+new BusMall('unicorn','./img/unicorn.jpg') ;
+new BusMall('water-can','./img/water-can.jpg') ;
+new BusMall('wine-galss','./img/wine-glass.jpg') ;
 
 
 
